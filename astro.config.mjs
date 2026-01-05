b637c01a818d2d54208d8d14c04540a7155f8b3f
Anikasomaia <anika.somaia@columbia.edu>
2026-01-05 14:13:09 -0800

    update seo gettings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon()],
+  site: "https://anikasomaia.com",
+  integrations: [tailwind(), icon(), sitemap()],
   output: "server",
-  adapter: vercel(),  // Changed from node() to vercel()
+  adapter: vercel(),
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
